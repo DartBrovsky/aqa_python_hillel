bb654ec02315d118afbb9479d67bc1999f0f4052
Serhii Stabroskyi <ukr5sams@gmail.com>
2024-07-18 20:05:09 +0300

    done: added lesson_1.py file

---
+++ lesson_1.py
@@ -0,0 +1,33 @@
+a = 20
+b = 29
+
+# a = b
+
+# first_variable_3 = ["1", "Hello"]
+# MONTH_FIRST = "Monday"
+# vkdsf = 1
+# firstVariable = 1
+#
+# print(MONTH_FIRST)
+# # print("Hello World")
+#
+#
+# if a == 20:
+#     print(a)
+#
+# print(b)
+
+summ = a + b  # Додавання
+
+print(summ)
+print(a + b)
+
+diff = a - b  # Віднімання
+
+print(diff)
+
+mult = a * b  # Множення
+print(mult)
+
+divd = a / b  # ділення
+print(divd)
